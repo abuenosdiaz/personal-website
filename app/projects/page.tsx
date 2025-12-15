af754cdef38fa1c22c7bfb6b23e84639ae0f3bf2
Ariel <adiaz13@bu.edu>
2025-12-15 00:43:55 -0500

    Add Button to Projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,19 @@
+'use client';
+import { useState } from "react"; 
+
 export default function Projects() {
-    return <h1>Projects</h1>
+    const [clicked, setClicked] = useState(false);
+
+    return (
+        <>
+            <h1>Projects</h1>
+            <button onClick={() => console.log("clicked")}>
+                Click
+            </button> 
+            {clicked && <p>Button was clicked</p>}
+        </>
+
+    )
 }
+
+
